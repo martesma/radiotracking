c5f434a083ba7402e8f083579e48946f588d704a
Larger One <inhortte@gmail.com>
2012-09-13 07:17:17 -0500

    top info line for animals

--- app/controllers/mapping_controller.rb
+++ app/controllers/mapping_controller.rb
@@ -1,6 +1,11 @@
 class MappingController < ApplicationController
   # params['id'] is the released animal id, not the radiotracking id.
   def show
+    @ra = begin
+            ReleasedAnimal.find(params['id'])
+          rescue
+            nil
+          end
     @rts = Radiotracking.find_all_by_released_animal_id(params['id'],
                                                         :order => [:date])
   end
